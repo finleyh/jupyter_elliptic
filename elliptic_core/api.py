dc5a4f44735492988af4da9b902b4c998a07abb9
Rogers Hornsby <rogers.hornsby@whitehouse.gov>
2023-09-28 11:49:50 -0400

    fleshing out API framework

--- elliptic_core/api.py
+++ elliptic_core/api.py
@@ -22,3 +22,58 @@ class API(object):
             if self.debug:
                 print(f'Attempted {method} to path {path} with data {json}')
         return self.session.request(method, full_url, json=json)
+    
+
+    def submit_wallet(self, data : str , asset : str = 'holistic', blockchain : str = 'blockchain', batch : bool = False):
+        if not batch:
+            path = '/v2/wallet/synchronous'
+        else:
+            path = '/v2/wallet'
+        method = 'POST'
+        payload = {
+            'subject':{
+                  'type':'address',
+                  'asset':asset,
+                  'blockchain':blockchain,
+                  'hash':data,
+            },
+            'type':'wallet_exposure'
+        }
+        return self.__results(method, path, payload)
+
+
+    def get_wallet(self, data : str):
+        method = 'GET'
+        path = f'/v2/wallet/{data}'
+        payload = None
+        return self.__results(method, path, payload)
+
+    def submit_transaction(self, data : str, type : str, customer_reference : str):
+        path  = '/v2/analyses'
+        method = 'POST'
+        payload = {
+            'subject':{
+                'type':'transaction',
+                'output_type':'address',
+                'asset':'holistic',
+                'blockchain':'holistic',
+                'hash':data,
+                'output_type':'address'
+            },
+            'type':type,
+            'customer_reference':customer_reference
+        }
+        return self.__results(method, path, payload)
+
+    def get_transaction(self, data : str):
+        method = 'GET'
+        path = f'/v2/analyses/{data}'
+        payload=None
+        return self.__results(method, path, payload)
+
+    def find_vasp_by_name(self, data : str):
+        print("TODO")
+
+    def find_vasp_by_id(self, data : str):
+        print("TODO")
+  
